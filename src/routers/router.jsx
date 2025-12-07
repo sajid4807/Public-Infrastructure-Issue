054e8ef3df8642bba132a94ae01307f7dd2cd1a8
sajid4807 <mdsajidhossen4807@gmail.com>
2025-12-08 00:14:22 +0600

    implement basic setup and create project

---
+++ src/routers/router.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router";
+
+ 
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="body-width">Hello World</div>,
+  },
+]);
